da4f1e8fe1edfe876e9de62c03c50f47190b98f3
magicBook <trade_offf@163.com>
2020-07-21 17:06:32 +0800

    add Page Route

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.scss';
 import { Layout } from 'antd';
+
+//引入路由需要的组件
+import { Switch, Route, Redirect } from 'dva/router';
+
 import NavBar from './NavBar';
-import Home from './Home'
+import Home from './Home';
+import About from './About';
+import Admin from './Admin';
+import Menus from './Menus';
+import Register from './User/Register';
+import Login from './User/Login';
 
 const { Header, Content } = Layout;
 
@@ -14,7 +23,19 @@ function IndexPage() {
         <NavBar />
       </Header>
       <Content className={styles.content}>
-        <Home></Home>
+        {/* 一级路由 */}
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/menus" component={Menus} />
+          <Route path="/admin" component={Admin} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+
+          {/* 重定向 */}
+          <Redirect to="/home" />
+        </Switch>
+
       </Content>
     </Layout>
   );
--- src/pages/NavBar/index.js
+++ src/pages/NavBar/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './index.scss'
 import { Menu } from 'antd'
+import { Link } from 'dva/router'
 
 export default function index() {
   return (
@@ -8,14 +9,14 @@ export default function index() {
       <a className={style.logo} href="http://www.baidu.com">lalal</a>
 
       <Menu className={style['menu-left']} mode="horizontal" defaultSelectedKeys={['home']}>
-          <Menu.Item key={'home'}>主页</Menu.Item>
-          <Menu.Item key={'menus'}>菜单</Menu.Item>
-          <Menu.Item key={'admin'}>管理</Menu.Item>
-          <Menu.Item key={'about'}>关于我们</Menu.Item>
-          <Menu.Item className={style.login} key={'login'}>登录</Menu.Item>
-          <Menu.Item className={style.register} key={'register'}>注册</Menu.Item>
+        <Menu.Item key={'home'}><Link to='/home'>主页</Link></Menu.Item>
+        <Menu.Item key={'menus'}><Link to='/menus'>菜单</Link></Menu.Item>
+        <Menu.Item key={'admin'}><Link to='/admin'>管理</Link></Menu.Item>
+        <Menu.Item key={'about'}><Link to='/about'>关于我们</Link></Menu.Item>
+        <Menu.Item className={style.login} key={'login'}><Link to='/login'>登录</Link></Menu.Item>
+        <Menu.Item className={style.register} key={'register'}><Link to='/register'>注册</Link></Menu.Item>
       </Menu>
-      
+
     </nav>
   )
 }
