f5fe76166ec884f5f50eee7c598710d22eefcdfa
magicBook <trade_offf@163.com>
2020-08-03 15:48:41 +0800

    route optimization

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -2,24 +2,18 @@ import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.scss';
 import { Layout } from 'antd';
-
 //引入路由需要的组件
-import { Switch, Redirect } from 'dva/router';
+import { Switch } from 'dva/router';
 
 import NavBar from './NavBar';
-// import Home from './Home';
-// import About from './About';
-// import Admin from './Admin';
-// import Menus from './Menus';
-// import Register from './User/Register';
-// import Login from './User/Login';
-import SubRoutes from '../utils/SubRoutes';
+import SubRoutes, { RedirectRoute, NoMatchRoute } from '../utils/SubRoutes';
 
 const { Header, Content } = Layout;
 
 
 function IndexPage(props) {
   const { routes } = props;
+  console.log(props)
   // console.log(props)
   return (
     <Layout className={styles.layout}>
@@ -29,19 +23,12 @@ function IndexPage(props) {
       <Content className={styles.content}>
         {/* 一级路由 */}
         <Switch>
-          {/* <Route path="/home" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/menus" component={Menus} />
-          <Route path="/admin" component={Admin} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} /> */}
-
           {routes.map((route, i) => (
             <SubRoutes key={i} {...route} />
           ))}
-
-          {/* 重定向 */}
-          <Redirect to="/home" />
+          <RedirectRoute exact={true} from={'/'} routes={routes}></RedirectRoute>
+          {/* 输入的连接不存在时，跳转到NoMatch组件中 */}
+          <NoMatchRoute />
         </Switch>
 
       </Content>
--- src/utils/SubRoutes.js
+++ src/utils/SubRoutes.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import { Route } from 'dva/router'
+import { Route, Redirect } from 'dva/router'
+import NoMatch from '../components/NoMatch'
 
 export default function SubRoutes({ routes, component: Component }) {
   return (
     <Route
       render={props => <Component {...props} routes={routes} />}
+    />
+  )
+}
+
+export function RedirectRoute({ routes, from, exact }) {
+  const routeR = routes.filter(item => {
+    return item.redirect;
+  })
+
+  const to = routeR.length ? routeR[0].path : routes[0].path;
+  return <Redirect exact={exact} from={from} to={to} />
+}
+
+export function NoMatchRoute({ status=404 }) {
+  return (
+    <Route
+      render={props => <NoMatch {...props} status={status} />}
     />
   )
 }
